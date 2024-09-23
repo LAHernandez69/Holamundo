e2ce971cdbc67d4c8c3e14de00c7268e83c1ca4d
LAHernandez69 <119449010+LAHernandez69@users.noreply.github.com>
2024-09-23 17:42:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Página cargada correctamente');
+document.getElementById('greetButton').addEventListener('click', function() {
+    const greetingMessage = document.getElementById('greetingMessage');
+    greetingMessage.textContent = '¡Hola, amigo! Bienvenido a mi página web.';
+    greetingMessage.classList.remove('hidden'); // Muestra el mensaje de saludo
+});
